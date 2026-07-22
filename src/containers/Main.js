977a276fd69999a0ce3a73a3403b1b81d0addbbd
Thariqhat Rama <thariqhatrama@gmail.com>
2026-07-22 14:38:00 +0700

    fix: switch from BrowserRouter to HashRouter to fix local SPA blank page reload

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, HashRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
 import Splash from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
@@ -32,7 +32,7 @@ export default class Main extends Component {
   }
   render() {
     return (
-      <BrowserRouter basename="/">
+      <HashRouter basename="/">
         <Switch>
           <Route
             path="/"
@@ -93,7 +93,7 @@ export default class Main extends Component {
             render={(props) => <Error404 {...props} theme={this.props.theme} />}
           />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
